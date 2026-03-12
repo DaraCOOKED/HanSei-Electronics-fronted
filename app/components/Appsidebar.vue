2f0fa3ecdc8d828fa5868a000d10789930c00f08
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2026-03-12 13:29:01 +0700

    feat/sidebar

--- app/components/Appsidebar.vue
+++ app/components/Appsidebar.vue
@@ -3,25 +3,33 @@
         <h1 class="text-4xl font-bold mt-6">Dashboard</h1>
 
         <ul class="space-y-4 mt-15">
-            <li class="hover:text-gray-500 cursor-pointer border-2 nowrap  ">
+
+            <li class="hover:text-gray-500 cursor-pointer">
+                <NuxtLink to="/dashboard">Dashboard</NuxtLink>
+            </li>
+
+            <li class="hover:text-gray-500 cursor-pointer">
                 <NuxtLink to="/employee-management">Employee Management</NuxtLink>
             </li>
 
-            <li class="hover:text-gray-500 cursor-pointer border-2 ">
+            <li class="hover:text-gray-500 cursor-pointer">
                 <NuxtLink to="/attendence-management">Attendence Management</NuxtLink>
             </li>
 
-            <li class="hover:text-gray-500 cursor-pointer border-2 ">
-                <NuxtLink to="/roster-management">roster-management</NuxtLink>
+            <li class="hover:text-gray-500 cursor-pointer">
+                <NuxtLink to="/roster-management">Roster Management</NuxtLink>
             </li>
 
-            <li class="hover:text-gray-500 cursor-pointer border-2 ">
+            <li class="hover:text-gray-500 cursor-pointer">
                 <NuxtLink to="/leave-management">Leave Management</NuxtLink>
             </li>
 
-            <li class="hover:text-gray-500 cursor-pointer border-2 ">
-                <NuxtLink to="/setting">setting</NuxtLink>
+            <li class="hover:text-gray-500 cursor-pointer">
+                <NuxtLink to="/setting">Setting</NuxtLink>
             </li>
+
+
+
         </ul>
     </div>
 </template>
---
+++ app/pages/setting.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Appsidebar from '~/components/Appsidebar.vue';
+</script>
+
+<template>
+    <div class="flex">
+
+        <Appsidebar />
+        <div class="mt-6 p-6 w-full">
+            <h2>setting</h2>
+
+        </div>
+
+    </div>
+
+</template>
