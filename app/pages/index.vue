aa00a3a42bb10803a604fcba7ee0342e07047f54
DaraCOOKED <dara.somnag020@gmail.com>
2026-03-16 14:54:06 +0700

    feat/i have to coplate some code in my page dashboard

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,13 +5,8 @@ import AppSidebar from '~/components/AppSidebar.vue'
 </script>
 
 <template>
-    <div class="flex">
 
         <AppSidebar />
 
-        <div class="p-6">
-            <h1>Dashboard</h1>
-        </div>
-
-    </div>
+        
 </template>
